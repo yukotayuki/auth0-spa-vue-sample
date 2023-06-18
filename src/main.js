25716a982f32beca5a2f1f3dd37120ac536f51a8
joo <yukota.yuki@hotmail.com>
2023-06-18 19:02:56 +0900

    feat: register vuetify plugin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createAuth0 } from '@auth0/auth0-vue'
 import App from './App.vue'
 import router from './router'
 import authConfig from '../.auth_config.json'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 app.use(router)
 app.use(
@@ -18,5 +24,6 @@ app.use(
     }
   })
 )
+app.use(vuetify)
 
 app.mount('#app')
