7ec58318efe58b8533bc17c2837f12dae070c09d
joo <yukota.yuki@hotmail.com>
2023-06-18 19:13:05 +0900

    feat: add v-card displaying user profile

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import NavigationBar from './components/NavigationBar.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Auth0User from './components/Auth0User.vue';
 </script>
 
 <template>
   <v-app>
     <navigation-bar></navigation-bar>
+    <v-main>
+      <auth0-user></auth0-user>
+    </v-main>
   </v-app>
 </template>
---
+++ src/components/Auth0User.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue'
+
+const { isAuthenticated, user }  = useAuth0()
+</script>
+
+<template>
+    <div class="mt-4"></div>
+    <v-card title="User Profile" color="#86c6da" theme="dark">
+        <v-card-text v-if="isAuthenticated">
+            <v-code>
+                {{ user }}
+            </v-code>
+        </v-card-text>
+        <v-card-text v-else>
+            User infomation will be displayed after login
+        </v-card-text>
+    </v-card>
+
+</template>
