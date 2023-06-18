0002ddad8bab3d16b6f47894e84afe17cd96fa64
joo <yukota.yuki@hotmail.com>
2023-06-18 16:34:56 +0900

    feat: register auth0 plugin

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue'
 import App from './App.vue'
 import router from './router'
+import authConfig from '../.auth_config.json'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(
+  createAuth0({
+    domain: authConfig.domain,
+    clientId: authConfig.clientId,
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })
+)
 
 app.mount('#app')
